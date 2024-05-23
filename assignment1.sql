981d6875035b448b0221064a4c8239d102525d81
kimmy\kimmy <wkmqyy@hotmail.com>
2024-05-23 09:29:15 -0400

    Insert value

--- assignment1.sql
+++ assignment1.sql
@@ -1,4 +1,3 @@
--- Step 1: Create a sample table
 CREATE TABLE Employees (
     EmployeeId INT,
     FirstName VARCHAR(100),
@@ -6,19 +5,23 @@ CREATE TABLE Employees (
     DepartmentName VARCHAR(100),
 );
 
--- Step 2: Insert some testing data
 INSERT INTO Employees (ID, FirstName, LastName, DepartmentName)
-VALUES (1, 'John', 'Doe', 'Support'), (2, 'Jane', 'Smith', 'Engineering'), (3, 'Bob', 'Johnson', 'Engineering'), (4, 'Alice', 'Brown', 'Support');
+VALUES (1, 'Ken', 'Ssnchez', 'Executive'), (2, 'Terri', 'Duffy', 'Engineering'),(3, 'Roberto', 'Tamburon', 'Engineering'), (4, 'Rob', 'Watson', 'Engineering'),(5, 'John', 'Doe', 'Support'), (6, 'Jane', 'Goldberg', 'Engineering'), (7, 'Dylan', 'Johnson', 'Support'), (8, 'Diane', 'Marghot', 'Support'), (9, 'Gigi', 'Mattew', 'Support'), (10, 'Michael', 'Raheem', 'Support');
 
--- Step 3: Use SQL functions in SELECT statement
+
 -- Use UPPER() function to convert last name to upper-case
 SELECT UPPER(LastName) AS UpperLastName, * FROM Employees;
+
 -- Use COUNT() function to count the number 
 SELECT COUNT(*) AS NumberOfEmployees FROM Employees;
+
 -- USe SUBSTRING() function to Extracts a substring from a string (starting at any position)
 SELECT SUBSTRING(FirstName, 1, 3) AS FirstThreeLetters FROM Employees;
 
+-- Use LOWER() function to convert department name to lower-case
+SELECT LOWER(DepartmentName) AS LowerDepartmentName FROM Employees;
 
 
+ 
 
 
